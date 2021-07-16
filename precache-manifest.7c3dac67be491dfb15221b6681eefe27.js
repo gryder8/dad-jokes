a13c922a87c5a0d5b645f29d62d069c65d372d21
gryder8 <gavin.ryder82@gmail.com>
2021-07-15 23:24:11 -0700

    Updates

---
+++ precache-manifest.7c3dac67be491dfb15221b6681eefe27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86036a04475ecc1f33dd62089a2c70bb",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "6505738be178a6f54d8b",
+    "url": "/dad-jokes/static/css/main.30e3c6ee.chunk.css"
+  },
+  {
+    "revision": "069b037659075bc567d6",
+    "url": "/dad-jokes/static/js/2.a3da951c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.a3da951c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6505738be178a6f54d8b",
+    "url": "/dad-jokes/static/js/main.386a1987.chunk.js"
+  },
+  {
+    "revision": "0ee283f3a45f81336623",
+    "url": "/dad-jokes/static/js/runtime-main.751de3d0.js"
+  }
+]);
